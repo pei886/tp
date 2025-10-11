836804b36e038050da8c2663720c57f3a31dc688
Estelle <e1354618@u.nus.edu>
2025-10-11 17:44:48 +0800

    Add Volunteer class

---
+++ src/main/java/loopin/projectbook/model/person/Volunteer.java
@@ -0,0 +1,47 @@
+package loopin.projectbook.model.person;
+
+import java.util.Set;
+
+import loopin.projectbook.model.tag.Tag;
+
+/**
+ * Represents a Volunteeer in the project book.
+ */
+public class Volunteer extends Person {
+
+    /**
+     * Name, email and tags must be present and non null but phone can be null.
+     */
+    public Volunteer(Name name, Phone phone, Email email, Set<Tag> tags) {
+        super(name, phone, email, null, tags);
+    }
+
+    /**
+     * Returns true if both persons are volunteers and have the same name.
+     * This defines a weaker notion of equality between two persons.
+     */
+    public boolean isSamePerson(Person otherPerson) {
+        if (otherPerson == this) {
+            return true;
+        } else if (!(otherPerson instanceof Volunteer)) {
+            return false;
+        }
+
+        return super.isSamePerson(otherPerson);
+    }
+
+    /**
+     * Returns true if both persons have the same identity and data fields.
+     * This defines a stronger notion of equality between two persons.
+     */
+    @Override
+    public boolean equals(Object other) {
+        // instanceof handles nulls
+        if (!(other instanceof Volunteer)) {
+            return false;
+        }
+
+        Person otherPerson = (Person) other;
+        return super.equals(otherPerson);
+    }
+}
