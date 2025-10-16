12e2a8ca05b4ae7dd3fa420f9c3df679e2ae21d6
jhshj886 <jingyapei@gmail.com>
2025-10-16 21:45:34 +0800

    Add project storage serialization

---
+++ src/main/java/loopin/projectbook/storage/JsonAdaptedProject.java
@@ -0,0 +1,78 @@
+package loopin.projectbook.storage;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import loopin.projectbook.commons.exceptions.IllegalValueException;
+import loopin.projectbook.model.project.Description;
+import loopin.projectbook.model.project.Project;
+import loopin.projectbook.model.project.ProjectName;
+
+/**
+ * Jackson-friendly version of {@link Project}.
+ */
+class JsonAdaptedProject {
+
+    public static final String MISSING_FIELD_MESSAGE_FORMAT = "Project's %s field is missing!";
+
+    private final String name;
+    private final String description;
+    private final LocalDateTime createdAt;
+    private final LocalDateTime updatedAt;
+
+    /**
+     * Constructs a {@code JsonAdaptedProject} with the given project details.
+     */
+    @JsonCreator
+    public JsonAdaptedProject(@JsonProperty("name") String name,
+                              @JsonProperty("description") String description,
+                              @JsonProperty("createdAt") LocalDateTime createdAt,
+                              @JsonProperty("updatedAt") LocalDateTime updatedAt) {
+        this.name = name;
+        this.description = description;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    /**
+     * Converts a given {@code Project} into this class for Jackson use.
+     */
+    public JsonAdaptedProject(Project source) {
+        name = source.getName().toString();
+        description = source.getDescription().toString();
+        createdAt = source.getCreatedAt();
+        updatedAt = source.getUpdatedAt();
+    }
+
+    /**
+     * Converts this Jackson-friendly adapted project object into the model's {@code Project} object.
+     *
+     * @throws IllegalValueException if there were any data constraints violated in the adapted project.
+     */
+    public Project toModelType() throws IllegalValueException {
+        if (name == null) {
+            throw new IllegalValueException(String.format(MISSING_FIELD_MESSAGE_FORMAT,
+                    ProjectName.class.getSimpleName()));
+        }
+        if (!ProjectName.isValidProjectName(name)) {
+            throw new IllegalValueException(ProjectName.MESSAGE_CONSTRAINTS);
+        }
+        final ProjectName modelName = new ProjectName(name);
+
+        if (description == null) {
+            throw new IllegalValueException(String.format(MISSING_FIELD_MESSAGE_FORMAT,
+                    Description.class.getSimpleName()));
+        }
+        if (!Description.isValidDescription(description)) {
+            throw new IllegalValueException(Description.MESSAGE_CONSTRAINTS);
+        }
+        final Description modelDescription = new Description(description);
+
+        return new Project(modelName, modelDescription);
+    }
+
+}
